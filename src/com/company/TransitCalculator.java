e9a2929cf1be3c6a05bd980f3b3ea397e9341842
Jim Gray <jimgray2005@gmail.com>
2020-02-13 22:59:20 -0500

    building out class constructor

---
+++ src/com/company/TransitCalculator.java
@@ -0,0 +1,33 @@
+package com.company;
+
+public class TransitCalculator {
+    int numberOfDays;
+    int numberOfRides;
+
+    /*
+        Pay-per-ride (single ride): $2.75
+        7-day Unlimited Rides: $33.00
+        30-day Unlimited Rides: $127.00
+    */
+    double singleRideRate = 2.75;
+    double sevenDayUnlimitedRideRate = 33.00;
+    double thirtyDayUnlimitedRideRate = 127.00;
+
+
+    public TransitCalculator(int days, int rides){
+        if(days <= 30){
+            numberOfDays = days;
+        } else {
+            numberOfDays = 30;
+        }
+
+        numberOfRides = rides;
+    }
+
+    public static void main(String[] args) {
+	    // write your code here
+        TransitCalculator myCalc = new TransitCalculator(6, 6);
+        System.out.println(myCalc.numberOfDays);
+        System.out.println(myCalc.numberOfRides);
+    }
+}
