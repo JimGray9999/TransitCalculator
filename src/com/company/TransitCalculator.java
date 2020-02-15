2a62062232fbd731786085023b5951aafcc79ae2
Jim Gray <jimgray2005@gmail.com>
2020-02-14 22:11:43 -0500

    unlimited 7 day ride method fixed

--- src/com/company/TransitCalculator.java
+++ src/com/company/TransitCalculator.java
@@ -1,8 +1,8 @@
 package com.company;
 
 public class TransitCalculator {
-    int numberOfDays;
-    int numberOfRides;
+    double numberOfDays;
+    double numberOfRides;
 
     /*
         Pay-per-ride (single ride): $2.75
@@ -13,13 +13,11 @@ public class TransitCalculator {
     double sevenDayUnlimitedRideRate = 33.00;
     double thirtyDayUnlimitedRideRate = 127.00;
 
-    public double unlimited7Price(int days, int rides){
-        double costPerRide = 0.00;
-
-        return costPerRide;
+    public double unlimited7Price(double days, double rides){
+        return Math.ceil(days / 7.0) * 33.00 / rides;
     };
 
-    public TransitCalculator(int days, int rides){
+    public TransitCalculator(double days, double rides){
         if(days <= 30){
             numberOfDays = days;
         } else {
@@ -31,8 +29,11 @@ public class TransitCalculator {
 
     public static void main(String[] args) {
 	    // write your code here
-        TransitCalculator myCalc = new TransitCalculator(6, 6);
-        System.out.println(myCalc.numberOfDays);
-        System.out.println(myCalc.numberOfRides);
+        TransitCalculator myCalc1 = new TransitCalculator(19.0, 20.0);
+        TransitCalculator myCalc2 = new TransitCalculator(22.0, 50.0);
+        TransitCalculator myCalc3 = new TransitCalculator(6.0, 14.0);
+        System.out.println(myCalc1.unlimited7Price(myCalc1.numberOfDays, myCalc1.numberOfRides));
+        System.out.println(myCalc2.unlimited7Price(myCalc2.numberOfDays, myCalc2.numberOfRides));
+        System.out.println(myCalc3.unlimited7Price(myCalc3.numberOfDays, myCalc3.numberOfRides));
     }
 }
