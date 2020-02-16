766cb6411dca3dbee623e402942c4482cc484d9f
Jim Gray <jimgray2005@gmail.com>
2020-02-15 22:07:44 -0500

    setup the best rate finder

--- src/com/company/TransitCalculator.java
+++ src/com/company/TransitCalculator.java
@@ -1,20 +1,42 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class TransitCalculator {
     double numberOfDays;
     double numberOfRides;
-
-    /*
-        Pay-per-ride (single ride): $2.75
-        7-day Unlimited Rides: $33.00
-        30-day Unlimited Rides: $127.00
-    */
     double singleRideRate = 2.75;
     double sevenDayUnlimitedRideRate = 33.00;
     double thirtyDayUnlimitedRideRate = 127.00;
 
     public double unlimited7Price(double days, double rides){
-        return Math.ceil(days / 7.0) * 33.00 / rides;
+        return Math.ceil(days / 7.0) * sevenDayUnlimitedRideRate / rides;
+    };
+
+    public double[] getRidePrices(double days, double rides){
+
+        double dailyRate = (rides * singleRideRate) / days;
+        double sevenDayRate = this.unlimited7Price(days, rides);
+        double unlimitedRate = thirtyDayUnlimitedRideRate / rides;
+        double[] costs = new double[] {dailyRate, sevenDayRate, unlimitedRate};
+        return costs;
+    };
+
+    public String getBestFare(double days, double rides){
+        double[] priceCompare = getRidePrices(days, rides);
+        //System.out.println(Arrays.toString(priceCompare));
+        String[] bestRateNames = new String[]{"Pay-per-ride","7-day Unlimited","30-day Unlimited"};
+        double bestPrice = priceCompare[0];
+        String  bestRate = bestRateNames[0];
+
+        for(int i = 0; i < priceCompare.length; i++){
+            if (bestPrice > priceCompare[i]){
+                bestPrice = priceCompare[i];
+                bestRate = bestRateNames[i];
+            }
+        };
+
+        return "You should get the " + bestRate + " option at " + "$" + String.format("%.2f", bestPrice) + " per ride.";
     };
 
     public TransitCalculator(double days, double rides){
@@ -25,15 +47,15 @@ public class TransitCalculator {
         }
 
         numberOfRides = rides;
-    }
+    };
 
     public static void main(String[] args) {
 	    // write your code here
         TransitCalculator myCalc1 = new TransitCalculator(19.0, 20.0);
         TransitCalculator myCalc2 = new TransitCalculator(22.0, 50.0);
         TransitCalculator myCalc3 = new TransitCalculator(6.0, 14.0);
-        System.out.println(myCalc1.unlimited7Price(myCalc1.numberOfDays, myCalc1.numberOfRides));
-        System.out.println(myCalc2.unlimited7Price(myCalc2.numberOfDays, myCalc2.numberOfRides));
-        System.out.println(myCalc3.unlimited7Price(myCalc3.numberOfDays, myCalc3.numberOfRides));
-    }
-}
+        System.out.println(myCalc1.getBestFare(myCalc1.numberOfDays, myCalc1.numberOfRides));
+        System.out.println(myCalc2.getBestFare(myCalc2.numberOfDays, myCalc2.numberOfRides));
+        System.out.println(myCalc3.getBestFare(myCalc3.numberOfDays, myCalc3.numberOfRides));
+    };
+};
